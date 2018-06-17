416c79be38d11541f8f029e29f7ee005845928db
Adam Haile <adammhaile@gmail.com>
2018-06-17 19:10:40 -0400

    Adding Teensy examples

---
+++ Examples/Teensy/Adafruit_NeoPixel/Adafruit_NeoPixel.ino
@@ -0,0 +1,61 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define NUM_LEDS 12
+
+// WingPixel uses the SPI MOSI pin for data output
+// so must be this pin!
+#define DATA_PIN 11
+
+//Typical color order for this LED type
+// If wrong, set back to RGB and the observer order on strip.
+// That order will be the correct order to place here.
+// i.e. if COLOR_ORDER set to RGB and output is B,G,G,R,R,R, set to BGR
+#define COLOR_ORDER NEO_GRB
+
+//decrease for testing over USB power
+#define BRIGHTNESS 64 // 25%
+
+//Setup strip object with above defines
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, DATA_PIN, COLOR_ORDER + NEO_KHZ800);
+
+uint32_t color_pattern[4] = {
+  0xFF0000,
+  0x00FF00,
+  0x0000FF,
+  0xFFFFFF
+};
+
+void setup() {
+  strip.begin(); // Initialize pins for output
+  strip.setBrightness(BRIGHTNESS);
+  strip.show();  // Turn all LEDs off ASAP
+}
+
+uint8_t c = 0;
+void loop(){
+  //Red, Green, Green, Blue, Blue, Blue
+  strip.setPixelColor(0, 0xFF0000);
+  strip.setPixelColor(1, 0x00FF00);
+  strip.setPixelColor(2, 0x00FF00);
+  strip.setPixelColor(3, 0x0000FF);
+  strip.setPixelColor(4, 0x0000FF);
+  strip.setPixelColor(5, 0x0000FF);
+
+  // Blank Separtor
+  strip.setPixelColor(6, 0x000000);
+
+  // Cycle through Red, Green, Blue, White
+  strip.setPixelColor(7, color_pattern[c]);
+  strip.setPixelColor(8, color_pattern[c]);
+  strip.setPixelColor(9, color_pattern[c]);
+
+  c++;
+  if(c>=4){ c=0; }
+
+  strip.show();
+  delay(1000);
+}
+
---
+++ Examples/Teensy/FastLED_WS2801/FastLED_WS2801.ino
@@ -0,0 +1,60 @@
+#include<FastLED.h>
+
+#define NUM_LEDS 12
+
+// WingPixel uses the SPI MOSI pin for data output
+// so must be this pin!
+#define DATA_PIN 11
+// WingPixel uses the SPI CLK pin for clock output
+// so must be this pin!
+#define CLOCK_PIN 13
+
+#define LEDTYPE WS2801
+
+//Typical color order for this LED type
+// If wrong, set back to RGB and the observer order on strip.
+// That order will be the correct order to place here.
+// i.e. if COLOR_ORDER set to RGB and output is B,G,G,R,R,R, set to BGR
+#define COLOR_ORDER RBG
+
+//decrease for testing over USB power
+#define BRIGHTNESS 64 // 25%
+
+CRGBArray<NUM_LEDS> leds;
+CRGBArray<4> color_pattern;
+
+void setup() {
+  color_pattern[0] = CRGB(255,0,0);
+  color_pattern[1] = CRGB(0,255,0);
+  color_pattern[2] = CRGB(0,0,255);
+  color_pattern[3] = CRGB(255,255,255);
+
+  FastLED.addLeds<LEDTYPE,DATA_PIN,CLOCK_PIN,COLOR_ORDER,DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+uint8_t c = 0;
+void loop(){
+  //Red, Green, Green, Blue, Blue, Blue
+  leds[0] = CRGB(255,0,0);
+  leds[1] = CRGB(0,255,0);
+  leds[2] = CRGB(0,255,0);
+  leds[3] = CRGB(0,0,255);
+  leds[4] = CRGB(0,0,255);
+  leds[5] = CRGB(0,0,255);
+
+  // Blank Separtor
+  leds[6] = CRGB(0,0,0);
+
+  // Cycle through Red, Green, Blue, White
+  leds[7] = color_pattern[c];
+  leds[8] = color_pattern[c];
+  leds[9] = color_pattern[c];
+
+  c++;
+  if(c>=4){ c=0; }
+
+  FastLED.show();
+  delay(1000);
+}
+
